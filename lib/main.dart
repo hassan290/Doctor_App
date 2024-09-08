a68c06e5b0a29e4e6a0126e8f53afc2ecec957c8
DESKTOP-5Q3Q4V7\Hassan <hassan258.shbib@gmail.com>
2024-09-08 10:41:17 +0300

    -update development branch after merging and repair conflict from features/onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:doctor_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-<<<<<<< HEAD
-  runApp(DocApp(appRouter: AppRouter(),));
-=======
-  //Comment
-  runApp(const MyApp());
->>>>>>> development
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
-

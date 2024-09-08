be63ddf76fac8ef2593f9040f044cf60245aea04
DESKTOP-5Q3Q4V7\Hassan <hassan258.shbib@gmail.com>
2024-09-08 22:20:36 +0300

    - added spacing file for SizeBox. - added some colors & textStyles. - added fontWieght helper file. - added general TextButton & general TextFormField - finished LoginScreen design

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,29 +1,77 @@
 import 'package:doctor_app/core/theming/colors.dart';
+import 'package:doctor_app/core/theming/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TTextStyles {
-  static TextStyle font24Black700Weight = TextStyle(
-    fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
-    color: Colors.black,
-  );
-
   static TextStyle font32BlueBold = TextStyle(
     fontSize: 32.sp,
     fontWeight: FontWeight.bold,
     color: TColors.mainBlue,
   );
 
-  static TextStyle font10GrayRegular = TextStyle(
-    fontSize: 13.sp,
-    fontWeight: FontWeight.normal,
-    color: TColors.grey,
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: TFontWeight.bold,
+    color: Colors.black,
+  );
+
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: TFontWeight.bold,
+    color: TColors.mainBlue,
   );
 
   static TextStyle font16WhiteSemiBold = TextStyle(
     fontSize: 16.sp,
-    fontWeight: FontWeight.w600,
+    fontWeight: TFontWeight.semiBold,
     color: Colors.white,
+  );
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: TFontWeight.medium,
+    color: TColors.darkBlue,
+  );
+
+  static TextStyle font14GrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: TFontWeight.normal,
+    color: TColors.grey,
+  );
+  static TextStyle font13GrayRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.normal,
+    color: TColors.lightGray,
+  );
+
+  static TextStyle font13BlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.normal,
+    color: TColors.mainBlue,
+  );
+
+  static TextStyle font13DarkBlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.normal,
+    color: TColors.darkBlue,
+  );
+
+  static TextStyle font13DarkBlueMedium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.medium,
+    color: TColors.darkBlue,
+  );
+
+  static TextStyle font13BlueSemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.semiBold,
+    color: TColors.mainBlue,
+  );
+
+  static TextStyle font10GrayRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: TFontWeight.normal,
+    color: TColors.grey,
   );
 }

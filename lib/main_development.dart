38d5a042efaba36f604cc35d359f68cbae298119
DESKTOP-5Q3Q4V7\Hassan <hassan258.shbib@gmail.com>
2024-09-17 02:53:30 +0300

    - added development & production flavors for Android. - changed android app name for each flavor. - added launch.json for flavor coding. - added two main filies for flavors.

---
+++ lib/main_development.dart
@@ -0,0 +1,18 @@
+import 'package:doctor_app/core/di/dependency_injection.dart';
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/doc_app.dart';
+import 'package:doctor_app/features/bloc_observer/bloc_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  //To fix text being hidden bug in flutter screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
+  await setupGetIt();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}

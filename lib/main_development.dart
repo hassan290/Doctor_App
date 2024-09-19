2ee83ef62c8bed2216f9e527705f3650f0f1fcf9
DESKTOP-5Q3Q4V7\Hassan <hassan258.shbib@gmail.com>
2024-09-20 01:06:42 +0300

    - added development & production flavor for Android - added launch.json for flavors in vscode - added two main filies for flavors

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,11 +4,13 @@ import 'package:doctor_app/doc_app.dart';
 import 'package:doctor_app/features/bloc_observer/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
